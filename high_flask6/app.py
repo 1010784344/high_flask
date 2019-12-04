c599f754d67f76ab697c2befa76de9b8f9b20888
caiguojing <1010784344@qq.com>
2019-12-04 12:59:28 +0800

    jinjia2 中的全局和局部变量

---
+++ high_flask6/app.py
@@ -0,0 +1,17 @@
+# 宏 有关的demo
+
+from flask import Flask,render_template
+
+app = Flask(__name__)
+# 模板中的代码修改之后会自动加载
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route('/myworld/')
+def index():
+
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
