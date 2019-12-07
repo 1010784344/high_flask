46fec5255bd49f8ba7fb1826f07f54bb793809c5
caiguojing <1010784344@qq.com>
2019-12-07 08:01:39 +0800

    2

--- high_flask9/app1.py
+++ high_flask9/app1.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 class Login(views.MethodView):
+    # 为了保持 get 函数书写格式的标准性，就把这个有传参处理的 render_template 函数抽取出来
+    def __render(self,error=None):
+        return render_template('login.html', error=error)
 
-    def get(self,error=None):
-        return render_template('login.html',error=error)
+    def get(self):
+        self.__render()
 
     def post(self):
         username =  request.form.get('username')
@@ -16,15 +19,13 @@ class Login(views.MethodView):
         if username == 'wangzhe' and password == '123456':
             return '登录成功'
         else:
-            # return render_template('login.html',error = '用户名或者密码错误')
-            return self.get(error='用户名或者密码错误')
-
-
+            self.__render(error='用户名或者密码错误')
 
 
 @app.route('/myworld/')
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
 
 
 app.add_url_rule('/login/',view_func=Login.as_view('login'))
