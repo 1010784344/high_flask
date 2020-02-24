f6e817bea336792d37dcf18be53f4b644b477dad
caiguojing <1010784344@qq.com>
2020-02-24 19:00:15 +0800

    sweetalert 和 flask 结合，优化修改密码弹窗

---
+++ zlbbs11/config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import os
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost/zlbbs?charset=utf8'
+# SQLALCHEMY 里面的模型一有变动，那么他都会给我们发送一个信号，没有必要会浪费性能
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# session 加密
+SECRET_KEY = 'man man lai'
+
+# 将 cms 后台用来保存 user.id 的键，提取为一个全局变量
+CMS_USER_ID = 'cms_user_id'
