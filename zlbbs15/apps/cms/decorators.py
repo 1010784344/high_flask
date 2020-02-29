710823cc45e85d6c079602759b559efd181ebccc
caiguojing <1010784344@qq.com>
2020-02-29 12:17:58 +0800

    cms 基于不同权限用户的页面内容显示(权限判断的功能)

---
+++ zlbbs15/apps/cms/decorators.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from flask import session,redirect,url_for
+import config
+from functools import wraps
+
+
+# 定义登录限制装饰器
+def LoginRequired(func):
+    @wraps(func)
+    def inner(*args,**kwargs):
+
+        # 判断 session 中是否存在某个 key
+        # if 'user_id' in session:
+        if config.CMS_USER_ID in session:
+            return func(*args,**kwargs)
+        else:
+            # 使用 url_for 反转 基于调度方法及蓝图的视图函数
+            return redirect(url_for('cms.login'))
+    return inner
+
+
+
