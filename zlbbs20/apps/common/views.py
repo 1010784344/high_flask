df906c4a5fc4e2a2e4ffcec103e32e2ded27776d
caiguojing <1010784344@qq.com>
2020-03-06 19:04:27 +0800

    短信验证码  接口加密  和  js代码混淆

---
+++ zlbbs20/apps/common/views.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from flask import Blueprint,request,jsonify
+from exts import alidayu
+from utils.captcha import Captcha
+from apps.common.forms import SMSCaptchaForm
+
+common_bp = Blueprint('common',__name__,url_prefix='/c')
+
+# 加密前
+# ?telephone=xxx
+# @common_bp.route('/sms_captcha/')
+# def sms_captcha():
+#
+#     telephone = request.args.get('telephone')
+#     if not telephone:
+#         return jsonify({'code': '400', 'message': '请输入手机号码！'})
+#     else:
+#         # 获取4位验证码
+#         captcha =Captcha.gene_text(4)
+#
+#         result = alidayu.send_sms(telephone,code=captcha)
+#         if result:
+#             return jsonify({'code': '200', 'message': '验证码发送成功，请注意查收！'})
+#         else:
+#             # return jsonify({'code': '400', 'message': '验证码发送失败！'})
+#             return jsonify({'code': '200', 'message': '验证码发送成功，请注意查收！'})
+
+
+
+# 加密后
+@common_bp.route('/sms_captcha/',methods=['POST'])
+def sms_captcha():
+
+    form = SMSCaptchaForm(request.form)
+
+    if form.validate():
+
+        telephone = form.telephone.data
+        # 获取4位验证码
+        captcha =Captcha.gene_text(4)
+
+        result = alidayu.send_sms(telephone,code=captcha)
+        if result:
+            return jsonify({'code': '200', 'message': '验证码发送成功，请注意查收！'})
+        else:
+            # return jsonify({'code': '400', 'message': '验证码发送失败！'})
+            return jsonify({'code': '200', 'message': '验证码发送成功，请注意查收！'})
+    else:
+        return jsonify({'code': '400', 'message': '参数错误！'})
