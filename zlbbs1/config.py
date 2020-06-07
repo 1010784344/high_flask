0f942eca6e6e0ac8b20931fa0b718652d69bfb55
caiguojing <1010784344@qq.com>
2020-06-07 17:14:05 +0800

    普通优化

--- zlbbs1/config.py
+++ zlbbs1/config.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost/zlbbs?charset=utf8'
 # SQLALCHEMY 里面的模型一有变动，那么他都会给我们发送一个信号，没有必要会浪费性能
--- zlbbs1/zlbbs.py
+++ zlbbs1/zlbbs.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask
 
 from apps.cms.views import cms_bp
